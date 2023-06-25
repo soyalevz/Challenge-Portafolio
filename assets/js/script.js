6f8b1fc17baa3952364c6342e3b9c2f0cad9e61f
José Alejandro Velásquez Sánchez <alevzdeveloper@gmail.com>
2023-06-25 19:10:36 -0400

    Portafolio

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar () {
+
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+
+
+
